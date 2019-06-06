e73e383697c747bc24695ca7a3566b67ce4ba9b6
Omid Golparvar <iGolparvar@Gmail.com>
2019-06-06 18:38:09 +0430

    V1.2 Editted

--- src/Data/Versions/V1.2.js
+++ src/Data/Versions/V1.2.js
@@ -16,23 +16,41 @@ function V1_2() {
     </article>
 
     <article className="uk-article">
-    <h1 className='uk-h2'>استفاده از <code dir='ltr'>if let</code>چندگانه</h1>
-    <p>الان می‌تونیم چندین <code dir='ltr'>if let</code> رو در یک خط بنویسیم و با استفاده از کاما اونا رو از هم جدا کنیم، و با استفاده از این کار، از تورفتگی‌های اضافی در کد جلوگیری کنیم.
-    </p>
-    <p>برای مثال ممکنه قبلا کدی شبیه به کد زیر داشته باشین:</p>
-    <pre>
-      <code className="swift">{`if let user = loadUsername() {
-          if let password = decryptPassword() {
-              authenticate(user, password)
-          }
-      }`}</code>
-    </pre>
-    <p>از الان می‌ةونیم به این صورت بنویسیمش:</p>
-    <pre>
-      <code className="swift">{`if let user = loadUsername(), let password = decryptPassword() {
-          authenticate(user, password)
-      }`}</code>
-    </pre>
+      <h1 className='uk-h2'>ساختمان داده جدید <code dir='ltr'>Set</code></h1>
+      <p>سوییفت ۱.۲ ساختار جدید <code dir='ltr'>Set</code> رو معرفی کرد که مشابه NSSet عمل می‌کنه بجز در Value Semantic. Set (یا همون مجموعه خودمون) مثل آرایه عمل می‌کنه با این تفاوت که عضو تکراری نمی‌پذیره و همچنین المان‌هاش ترتیب خاصی ندارن. بعنوان مثال:</p>
+      <pre>{`var starships = Set<String>()
+        starships.insert("Serenity")
+        starships.insert("Enterprise")
+        starships.insert("Executor")
+        starships.insert("Serenity")
+        starships.insert("Serenity")`}
+      </pre>
+      <p>با اینکه این کد چندین بار سعی در اضافه‌کردن Serenity می‌کنه، اما این مقدار فقط یک‌بار داخل Set ذخیره میشه.</p>
+    </article>
+    <article className="uk-article">
+      <h1 className='uk-h2'>رویه Bridging ضمنی کاهش پیدا کرده</h1>
+      <p>تا قبل از سوییفت ۱.۲ Bridging تایپ‌های Objective-C به تایپ‌های سوییفت بصورت ضمنی انجام میشد، به این معنی که اون تایپ‌ها رو بطور تبادل‌پذیر می‌تونستیم استفاده کنیم. از سوییفت ۱.۲ این روند باید توسط خود برنامه‌نویس و با استفاده از <code dir='ltr'>as</code> انجام بشه. بطور مثال:</p>
+      <pre>{`authenticateUser(yourNSString as String)`}</pre>
+    </article>
+
+    <article className="uk-article">
+      <h1 className='uk-h2'>استفاده از <code dir='ltr'>if let</code>چندگانه</h1>
+      <p>الان می‌تونیم چندین <code dir='ltr'>if let</code> رو در یک خط بنویسیم و با استفاده از کاما اونا رو از هم جدا کنیم، و با استفاده از این کار، از تورفتگی‌های اضافی در کد جلوگیری کنیم.
+      </p>
+      <p>برای مثال ممکنه قبلا کدی شبیه به کد زیر داشته باشین:</p>
+      <pre>
+        <code className="swift">{`if let user = loadUsername() {
+            if let password = decryptPassword() {
+                authenticate(user, password)
+            }
+        }`}</code>
+      </pre>
+      <p>از الان می‌ةونیم به این صورت بنویسیمش:</p>
+      <pre>
+        <code className="swift">{`if let user = loadUsername(), let password = decryptPassword() {
+            authenticate(user, password)
+        }`}</code>
+      </pre>
     </article>
 
     <article className="uk-article">
