1d904f6043386a7627c844f7d9ae380a5465a347
Omid Golparvar <igolparvar@gmail.com>
2020-03-30 21:11:02 +0430

    AllVersions Changed

--- src/Data/AllVersions.js
+++ src/Data/AllVersions.js
@@ -1,22 +1,37 @@
 import V1_0 from './Versions/V1.0.js'
 import V1_1 from './Versions/V1.1.js'
 import V1_2 from './Versions/V1.2.js'
-// import V2_0 from './Versions/V2.0.js'
-// import V2_1 from './Versions/V2.1.js'
-// import V2_2 from './Versions/V2.2.js'
-// import V3_0 from './Versions/V3.0.js'
-// import V3_1 from './Versions/V3.1.js'
-// import V4_0 from './Versions/V4.0.js'
+import V2_0 from './Versions/V2.0.js'
+import V2_1 from './Versions/V2.1.js'
+import V2_2 from './Versions/V2.2.js'
+import V3_0 from './Versions/V3.0.js'
+import V3_1 from './Versions/V3.1.js'
+import V4_0 from './Versions/V4.0.js'
 import V4_1 from './Versions/V4.1.js'
-// import V4_2 from './Versions/V4.2.js'
-// import V5_0 from './Versions/V5.0.js'
-// import V5_1 from './Versions/V5.1.js'
+import V4_2 from './Versions/V4.2.js'
+import V5_0 from './Versions/V5.0.js'
+import V5_1 from './Versions/V5.1.js'
+import V5_2 from './Versions/V5.2.js'
 
 const AllVersions = [
   V1_0,
   V1_1, 
   V1_2,
-  V4_1
+
+  V2_0,
+  V2_1,
+  V2_2,
+
+  V3_0,
+  V3_1,
+
+  V4_0,
+  V4_1,
+  V4_2,
+
+  V5_0,
+  V5_1,
+  V5_2,
 ]
 
 export default AllVersions
